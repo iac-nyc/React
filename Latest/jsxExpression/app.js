9e2b2881c84dc62db497cfdb01379b9a5b254f63
Iftekhar A Chowdhury <007ifte@gmail.com>
2018-11-21 08:53:53 -0500

    JSX_Expression

---
+++ Latest/jsxExpression/app.js
@@ -0,0 +1,16 @@
+
+
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+const myTitleID = 'main-title';
+const name = 'Iftekhar Ahmad Chowdhury';
+const header = (
+    <header>
+        <h1 id={myTitleID}>{ name }'s First React Element</h1>
+        <p>{ desc }</p>
+    </header>
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
